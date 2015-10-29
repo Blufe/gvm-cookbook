42f1462fb72b5e7310d0578befc152a31da7faa6
Blufe <bluephoenixlab@gmail.com>
2015-10-29 13:11:06 +0900

    Support CentOS

--- recipes/_package.rb
+++ recipes/_package.rb
@@ -1,7 +1,18 @@
-include_recipe "apt"
+case node["platform"]
 
-%w(curl git mercurial make binutils bison gcc).each do |pkg|
-  package pkg do
-    action :install
+when "debian", "ubuntu"
+  include_recipe "apt"
+
+  %w(curl git mercurial make binutils bison gcc).each do |pkg|
+    package pkg do
+      action :install
+    end
+  end
+
+when "redhat", "centos", "fedora", "amazon"
+  %w(curl git make bison gcc glibc-devel).each do |pkg|
+    package pkg do
+      action :install
+    end
   end
 end
